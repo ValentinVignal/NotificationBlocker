4dc86211220527096454feef483837c7e2f67e24
ValentinVignal <valentin.vignal.dev@outlook.fr>
2026-08-12 20:40:58 +0800

    :rocket: 1.6

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## 1.6
+
+- Update targetSdk to 36
+- Update UI to be more M3 like
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.valentinvignal.notificationblocker"
         minSdk = 34
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 6
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
